1f83ec37d24e46b1261459e813772e22255f6206
Avetome <avetome@gmail.com>
2019-05-26 00:55:13 +0300

    Move example data generate logic to separate project

---
+++ BooleanRetrieval.Logic/DataGenerating/ExampleDataGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BooleanRetrieval.Logic.DataGenerating
+{
+    public static class ExampleDataGenerator
+    {
+        public static void Generate(string fileWithOriginData, int dataCount)
+        {
+            var notebooks = new List<string>();
+
+            var notebooksInFileCount = ReadNotebooks(fileWithOriginData, ref notebooks);
+
+            var newfilename = $"notebooks_{dataCount}.csv";
+            var rnd = new Random();
+            var idHash = new HashSet<int>();
+
+            var j = 0;
+
+            using (StreamWriter file = new StreamWriter(newfilename))
+            {
+                for (var i = 0; i < dataCount; i++)
+                {
+                    if (j == notebooksInFileCount)
+                    {
+                        j = 0;
+                    }
+
+                    var id = rnd.Next(0, 90000000);
+
+                    while (idHash.Contains(id))
+                    {
+                        id = rnd.Next(0, 90000000);
+                    }
+
+                    idHash.Add(id);
+
+                    file.WriteLine($"{id},{notebooks[j++]}");
+                }
+            }
+        }
+
+        private static int ReadNotebooks(string filename, ref List<string> notebooks)
+        {
+            int counter = 0;
+            string line;
+
+            using (StreamReader file = new StreamReader(filename))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    var i = 0;
+                    while (line[i] != ',' && i < line.Length)
+                    {
+                        i++;
+                    }
+
+                    notebooks.Add(line.Substring(++i));
+
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
--- BooleanRetrieval/Program.cs
+++ BooleanRetrieval/Program.cs
@@ -1,5 +1,5 @@
 ﻿using BooleanRetrieval.Logic.Indexing;
-using BooleanRetrieval.Logic.QueryParsing;
+using BooleanRetrieval.Logic.DataGenerating;
 using BooleanRetrieval.Logic.Searching;
 using System;
 using System.Collections.Generic;
@@ -36,7 +36,7 @@ namespace BooleanRetrieval
             {
                 if (int.TryParse(args[1], out var number))
                 {
-                    GenerateDataFile(Filename, number);
+                    ExampleDataGenerator.Generate(Filename, number);
                     return;
                 }
             }
@@ -167,67 +167,6 @@ namespace BooleanRetrieval
 
             Console.WriteLine();
             Console.WriteLine($"Total results: {result.Count}. Search time: {stopWatch.ElapsedMilliseconds} ms.");
-        }
-
-        private static void GenerateDataFile(string filename, int count)
-        {
-            Console.WriteLine($"Generating data file with {count} notebooks");
-
-            var notebooks = new List<string>();
-
-            var notebooksInFileCount = ReadNotebooks(filename, ref notebooks);
-
-            var newfilename = $"notebooks_{count}.csv";
-            var rnd = new Random();
-            var idHash = new HashSet<int>();
-
-            var j = 0;
-
-            using (StreamWriter file = new StreamWriter(newfilename))
-            {
-                for (var i = 0; i < count; i++)
-                {
-                    if (j == notebooksInFileCount)
-                    {
-                        j = 0;
-                    }
-
-                    var id =  rnd.Next(0, 90000000);
-
-                    while(idHash.Contains(id))
-                    {
-                        id = rnd.Next(0, 90000000);
-                    }
-
-                    idHash.Add(id);
-
-                    file.WriteLine($"{id},{notebooks[j++]}");
-                }
-            }
-        }
-
-        private static int ReadNotebooks(string filename, ref List<string> notebooks)
-        {
-            int counter = 0;
-            string line;
-
-            using (StreamReader file = new StreamReader(filename))
-            {
-                while ((line = file.ReadLine()) != null)
-                {
-                    var i = 0;
-                    while (line[i] != ',' && i < line.Length)
-                    {
-                        i++;
-                    }
-
-                    notebooks.Add(line.Substring(++i));
-
-                    counter++;
-                }
-            }
-
-            return counter;
-        }
+        } 
     }
 }
