3809af8feb4c451ff63961d0f4e0251689eb04d4
Avetome <avetome@gmail.com>
2019-05-26 13:37:02 +0300

    Expression tree query parser

---
+++ BooleanRetrieval.Logic/QueryParsing/EtQueryParser.cs
@@ -0,0 +1,120 @@
+﻿using BooleanRetrieval.Logic.QueryParsing.Tokenize;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BooleanRetrieval.Logic.QueryParsing
+{
+    public class EtQueryParser
+    {
+        private TokenReader _tokenReader;
+
+        public ExpressionTreeNode Parse(string query)
+        {
+            _tokenReader = new TokenReader(query);
+
+            _tokenReader.NextToken();
+
+            var result = ParseAnd();
+
+            if (_tokenReader.Token != Token.EOL)
+            {
+                throw new Exception("Invalid search string format");
+            }
+
+            return result;
+        }
+
+        private ExpressionTreeNode ParseAnd()
+        {
+            var node = ParseOr();
+
+            if (_tokenReader.Token != Token.And)
+            {
+                return node;
+            }
+
+            _tokenReader.NextToken();
+
+            ExpressionTreeNode root = ExpressionTreeNode.CreateAnd(node, ParseOr());
+            node = root;
+
+            while (true)
+            {
+                if (_tokenReader.Token != Token.And)
+                {
+                    return root;
+                }
+
+                _tokenReader.NextToken();
+
+                var child1 = node.Child2;
+
+                node.Child2 = ExpressionTreeNode.CreateAnd(child1, ParseOr());
+
+                node = node.Child2;
+            }
+        }
+
+        private ExpressionTreeNode ParseOr()
+        {
+            var node = ParseNot();
+
+            _tokenReader.NextToken();
+
+            if (_tokenReader.Token != Token.Or)
+            {
+                return node;
+            }
+
+            _tokenReader.NextToken();
+
+            ExpressionTreeNode root = ExpressionTreeNode.CreateOr(node, ParseNot());
+            node = root;
+
+            _tokenReader.NextToken();
+
+            while (true)
+            {
+                if (_tokenReader.Token != Token.Or)
+                {
+                    return root;
+                }
+
+                _tokenReader.NextToken();
+
+                var child1 = node.Child2;
+
+                node.Child2 = ExpressionTreeNode.CreateOr(child1, ParseNot());
+
+                node = node.Child2;
+
+                _tokenReader.NextToken();
+            }
+        }
+
+        private ExpressionTreeNode ParseNot()
+        {
+            if (_tokenReader.Token != Token.Not)
+            {
+                return ParseTerm();
+            }
+
+            _tokenReader.NextToken();
+
+            var term = ParseTerm();
+
+            return ExpressionTreeNode.CreateNot(term);
+        }
+
+        private ExpressionTreeNode ParseTerm()
+        {
+            if (_tokenReader.Token != Token.Term)
+            {
+                throw new Exception("Invalid search string format");
+            }
+
+            return ExpressionTreeNode.CreateTerm(_tokenReader.Term);
+        }
+    }
+}
---
+++ BooleanRetrieval.Logic/QueryParsing/ExpressionTreeNode.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace BooleanRetrieval.Logic.QueryParsing
+{
+    [DebuggerDisplay("{string.IsNullOrEmpty(Term) ? Operation : Term}")]
+    public class ExpressionTreeNode
+    {
+        public string Operation { get; set; }
+
+        public string Term { get; set; }
+
+        public ExpressionTreeNode Child1 { get; set; }
+
+        public ExpressionTreeNode Child2 { get; set; }
+
+        public static ExpressionTreeNode CreateTerm(string term)
+        {
+            return new ExpressionTreeNode() { Term = term };
+        }
+
+        public static ExpressionTreeNode CreateNot(ExpressionTreeNode term)
+        {
+            return new ExpressionTreeNode() { Operation = "NOT", Child1 = term };
+        }
+
+        public static ExpressionTreeNode CreateOr(ExpressionTreeNode child1, ExpressionTreeNode child2)
+        {
+            return new ExpressionTreeNode() { Operation = "OR", Child1 = child1, Child2 = child2 };
+        }
+
+        public static ExpressionTreeNode CreateAnd(ExpressionTreeNode child1, ExpressionTreeNode child2)
+        {
+            return new ExpressionTreeNode() { Operation = "AND", Child1 = child1, Child2 = child2 };
+        }
+    }
+}
